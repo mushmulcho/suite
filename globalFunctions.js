10aa2b8e3eec94a413976b25a473965dc90f8ed4
mushmulcho <alex.goychev@gmail.com>
2015-08-17 19:42:55 +0300

    Create globalFunctions.js

---
+++ globalFunctions.js
@@ -0,0 +1,77 @@
+var scope = scope || {};
+
+(function (scope){
+
+    function GlobalFunctions () {
+        this.width = 90;
+        this.height = 135;
+        this.canvas = document.getElementById("canvas");
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+    GlobalFunctions.prototype.drawField = function (field) {
+        for (var i = 0; i < field.piles.length; i++) {
+            var pile = field.piles[i];
+            this.ctx.strokeStyle = "black 1px radius: 2px";
+            this.ctx.strokeRect(pile.x + 3, pile.y + 3, 84, 129);
+            for (var j = 0; j < pile.cards.length; j++) {
+                var card = pile.cards[j];
+                card.x = pile.x;
+                if (pile.cascading) {
+                    card.y = pile.y + j*20;
+                }
+                else {
+                    card.y = pile.y;
+                }
+
+                this.paintCard(card);
+
+            }
+        }
+    };
+
+
+    GlobalFunctions.prototype.loadCards = function (deck) {
+        for (var i = 0; i < deck.cards.length; i++) {
+            var img = new Image();
+            img.src = "Resources/" + deck.cards[i].rank + deck.cards[i].suit + ".png";
+            deck.cards[i].image= img;
+
+        }
+    };
+    GlobalFunctions.prototype.paintCard =function (card) {
+        var context = this.ctx;
+        var img = new Image();
+        if(card.face){
+            img.src = "Resources/" +card.rank + card.suit + ".png";
+        }
+        else{
+            img.src = "Resources/backlizard.png";
+        }
+
+        img.onload = function () {
+            context.drawImage(img, card.x, card.y);
+        }
+    } ;
+    GlobalFunctions.prototype.paintPile = function (pile) {
+        for (var i = 0; i < pile.cards.length; i++) {
+            this.paintCard(pile.cards[i]);
+        }
+    };
+
+
+    GlobalFunctions.prototype.drawAllCards = function(deck) {
+        for (var i = 0; i < deck.cards.length; i++) {
+            this.paintCard(deck.cards[i]);
+        }
+    };
+
+    GlobalFunctions.prototype.clearBoard = function () {
+        this.ctx.clearRect(0,0,1200,900);
+    };
+    scope.getGlobalFunctions = function () {
+        return new GlobalFunctions();
+    };
+
+
+})(scope);
